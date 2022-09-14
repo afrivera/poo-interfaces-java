b1c6c57d247a312ffcd39fdd825c68785e0fd5ff
afrivera <andresrfelip@hotmail.com>
2022-09-13 21:42:13 -0500

    nuevas caracteristicas de las interfaces a partir de java 8

--- src/org/afrivera/poointerfaces/imprenta/modelo/Imprimible.java
+++ src/org/afrivera/poointerfaces/imprenta/modelo/Imprimible.java
@@ -3,4 +3,17 @@ package org.afrivera.poointerfaces.imprenta.modelo;
 public interface Imprimible {
 
     String imprimir();
+
+    // a partir de java 8
+
+    String TEXTO = "TEXTO X";
+    // para no estar obligado a implementar un metodo se pone el default
+    default String noObligado(){
+        return "imprime valor por defecto";
+    };
+
+    // es posible un metodo estatico
+    static void impresion(Imprimible imprimible){
+        System.out.println("cualquier tontera");
+    }
 }
